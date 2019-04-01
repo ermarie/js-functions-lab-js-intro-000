c330936cb230b2cc7fc01d69c21936c0df86952f
Erin Stalzer <erinstalzer@gmail.com>
2019-04-01 22:33:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidaysTo(holiday, name){
   return "Happy ${holiday}, ${name}!"
 }
 
-function holidayCountdown(name, number){
+function holidayCountdown(holiday, days){
   return "It's ${days} days until ${holiday}!"
 }
